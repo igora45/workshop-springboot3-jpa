51a9910f1720fa5c5edeac45cb0dd755d79354d2
Igor Carvalho de Macedo Antônio <igordemacedo129@gmail.com>
2024-02-29 22:59:39 -0300

    fix: Correct function name in ResourceExceptionHandler

--- src/main/java/com/project/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/project/course/resources/exceptions/ResourceExceptionHandler.java
@@ -21,7 +21,7 @@ public class ResourceExceptionHandler {
         return ResponseEntity.status(status).body(err);
     }
     @ExceptionHandler(DatabaseException.class)
-    public ResponseEntity<StandardError> resourceNotFound(DatabaseException e, HttpServletRequest request){
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
         String error = "Database error!";
         HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
