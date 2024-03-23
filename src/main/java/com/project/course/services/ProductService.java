eb60dd6ce96c9abb38257f29e41d909438850996
Igor Carvalho de Macedo Antônio <igordemacedo129@gmail.com>
2024-03-23 02:27:32 -0300

    feat: Product insert/delete/update

--- src/main/java/com/project/course/services/ProductService.java
+++ src/main/java/com/project/course/services/ProductService.java
@@ -2,7 +2,12 @@ package com.project.course.services;
 
 import com.project.course.entities.Product;
 import com.project.course.repositories.ProductRepository;
+import com.project.course.services.exceptions.DatabaseException;
+import com.project.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,6 +26,36 @@ public class ProductService {
     public Product findById(Long id){
         Optional<Product> obj = repository.findById(id);
         return obj.get();
+    }
+
+    public Product insert (Product obj){
+        return repository.save(obj);
+    }
+
+    public void delete(Long id){
+        try{
+            repository.deleteById(id);
+        } catch(EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch(DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public Product update (Long id, Product obj){
+        try{
+            Product entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    public void updateData(Product entity, Product obj){
+        entity.setName(obj.getName());
+        entity.setDescription(obj.getDescription());
+        entity.setPrice(obj.getPrice());
     }
 
 }
