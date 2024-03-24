8ca7558b7da59dcd44fc412c37381180be943feb
Igor Carvalho de Macedo Antônio <igordemacedo129@gmail.com>
2024-03-24 01:42:32 -0300

    feat: Order insert/delete

--- src/main/java/com/project/course/services/OrderService.java
+++ src/main/java/com/project/course/services/OrderService.java
@@ -1,8 +1,14 @@
 package com.project.course.services;
 
 import com.project.course.entities.Order;
+import com.project.course.entities.enums.OrderStatus;
 import com.project.course.repositories.OrderRepository;
+import com.project.course.services.exceptions.DatabaseException;
+import com.project.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,6 +27,20 @@ public class OrderService {
     public Order findById(Long id){
         Optional<Order> obj = repository.findById(id);
         return obj.get();
+    }
+
+    public Order insert(Order obj){
+        return repository.save(obj);
+    }
+
+    public void delete(Long id){
+        try {
+            repository.deleteById(id);
+        } catch(EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch(DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
 }
